8c50a861fd390c7f6d76fee27fea776b8e384501
Tim Kiley <tiki9774@colorado.edu>
2023-04-27 20:37:48 -0700

    fixing problems

--- main.m
+++ main.m
@@ -1,14 +1,23 @@
 % Time Kalmen Filter
-fc = 10 %Hz
-t = linspace(0,1,10000)
-x1 = sin(2*pi*fc*t);
-x2 = cos(2*pi*fc*t);
 
-x3 = x1 + x2
 
-figure;
-plot(x1)
+f = [1 10 100 1e3 10e3 100e3]
+L = [-50 -70 -113 -128 -135 -140];
+
+sigma = L_F_2_sigma(f,L)
+
+f0 = 10e6;
+
+t = linspace(0,1,1000);
+
+phase = 2*pi*f0*t;
+n = sigma .* randn(1,length(t))
+
+phase_n = phase + n;
+
+[p,S] = polyfit(t,phase_n,1);
+tmp = polyval(p,t);
+figure
+plot(t,sin(phase_n),'o')
 hold on;
-plot(x2)
-hold on;
-plot(x3)
+plot(t,sin(tmp))
